b8045cdab6bf7119c81777a6246aba4bf1d8485b
imdjsarge <rgallo001@my.hennepintech.edu>
2015-11-04 20:41:45 -0600

    basic ajax started

--- js/script.js
+++ js/script.js
@@ -1,36 +1,40 @@
-( function() {
+(function () {
 
 
 
-  var coffeeShopApp = {
-      buildMenu : function( data ) {
+    var coffeeShopApp = {
+        buildMenu: function (data) {
 
-          var $menu = $("#menu");
-          $("<h3>Beverages</h3>").insertAfter($menu.children().last());
-          $('<ul id="espresso">').insertAfter($menu.children().last());
+            var $menu = $("#menu");
+            $("<h3>Beverages</h3>").insertAfter($menu.children().last());
+            $('<ul id="espresso">').insertAfter($menu.children().last());
 
-          $("<h3>Bakery</h3>").insertAfter($menu.children().last());
-          $('<ul id="bakery">').insertAfter($menu.children().last());
+            $("<h3>Bakery</h3>").insertAfter($menu.children().last());
+            $('<ul id="bakery">').insertAfter($menu.children().last());
 
-          // Add code to get the menu data using Ajax
-          // & add the menu items to the appropriate section
-          // An example:
-          // <li><span class="menu-item">Cappuccino $4.00</span><img src="images/cappuccino.jpg"></li>
+            // Add code to get the menu data using Ajax
+            // & add the menu items to the appropriate section
+            // An example:
+            // <li><span class="menu-item">Cappuccino $4.00</span><img src="images/cappuccino.jpg"></li>
 
-      }
+        }
 
+      
+
+        // Add a method to validate the registration form
+        // There should be both a name and a valid email
+
+    }
+
+    // Uncomment the line below to build the menu
+    //coffeeShopApp.buildMenu();
+
+    // Call the methods to add the weekly specials
+
+    // Add code for the form submit button it should validate the form
+    
       // Add a new method to add the weekly specials
+        $("<p>").text("Bakery Goods 15% off!").insertAfter("#specials");
+        $("<p>").text("Drink of the week: Double Mint Mocha $3").insertAfter("#specials");
 
-      // Add a method to validate the registration form
-      // There should be both a name and a valid email
-
-  }
-
-  // Uncomment the line below to build the menu
-  //coffeeShopApp.buildMenu();
-
-  // Call the methods to add the weekly specials
-
-  // Add code for the form submit button it should validate the form
-
-}())
+}());
