598c7e2a9969849720d1e567be05bfd0d72b1db4
imdjsarge <rgallo001@my.hennepintech.edu>
2015-11-04 21:12:06 -0600

    jquery ajax added

--- js/script.js
+++ js/script.js
@@ -11,8 +11,22 @@
 
             $("<h3>Bakery</h3>").insertAfter($menu.children().last());
             $('<ul id="bakery">').insertAfter($menu.children().last());
+            
+            
 
             // Add code to get the menu data using Ajax
+             $.ajax({
+                type: "GET",
+                 url: "data/menu.json",
+                 dataType: "json",
+                 success: function(buildMenu) {
+                     console.log(coffeeShopApp.buildMenu);
+                 },
+                 error: function() {
+                     alert ("an ajax error occured.");
+                 }                             
+             });
+             
             // & add the menu items to the appropriate section
             // An example:
             // <li><span class="menu-item">Cappuccino $4.00</span><img src="images/cappuccino.jpg"></li>
@@ -22,19 +36,21 @@
       
 
         // Add a method to validate the registration form
-        // There should be both a name and a valid email
+            
+        
+        // There should be both a name and a valid email (cannot be a blank email or the email is not in proper form!!!
 
     }
 
     // Uncomment the line below to build the menu
-    //coffeeShopApp.buildMenu();
+    coffeeShopApp.buildMenu();
 
     // Call the methods to add the weekly specials
 
     // Add code for the form submit button it should validate the form
     
       // Add a new method to add the weekly specials
-        $("<p>").text("Bakery Goods 15% off!").insertAfter("#specials");
-        $("<p>").text("Drink of the week: Double Mint Mocha $3").insertAfter("#specials");
+    // $("<p>").text("Bakery Goods 15% off!").insertAfter("#specials");
+       // $("<p>").text("Drink of the week: Double Mint Mocha $3").insertAfter("#specials");
 
 }());
